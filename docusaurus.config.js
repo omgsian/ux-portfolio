39b9e2a31562bd6196d3cb0bce8ba44c94e3c508
Ridzwan Haron <omgsian@ridzwans-mbp-2.lan>
2022-08-10 16:26:03 +0800

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,10 +5,10 @@ const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
-const config = {
+const config = {  
   title: 'Blossom',
   tagline: 'Thoughts about UX',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://elegant-dasik-55c6c4.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -117,7 +117,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Ridzwan. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Ridzwan.`,
       },
       prism: {
         theme: lightCodeTheme,
@@ -129,7 +129,122 @@ const config = {
 // module.exports = config;
 
 module.exports = {
-  url: 'https://elegant-dasik-55c6c4.netlify.app', // Url to your site with no trailing slash
-  baseUrl: '/', // Base directory of your site relative to your repo
-  title: 'Portfolio',
+  title: 'Blossom',
+  tagline: 'Thoughts about UX',
+  url: 'https://elegant-dasik-55c6c4.netlify.app',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'omgsian', // Usually your GitHub org/user name.
+  projectName: 'ridzwanblog', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/omgsian/ux-portfolio/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/omgsian/ux-portfolio/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Ridzwan Haron',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/undraw_wanderer.svg ',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'About',
+          },
+          {to: '/portfolio', label: 'Portfolio', position: 'left'},
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/omgsian',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Social',
+            items: [
+              {
+                label: 'Linkedin',
+                href: 'https://www.linkedin.com/in/ridzwanharon/',
+              },
+              {
+                label: 'Github',
+                href: 'https://github.com/omgsian',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/#',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Ridzwan.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
