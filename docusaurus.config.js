e4c71d08892385fe4155f151df033a921eb1cf1e
Ridzwan Haron <omgsian@ridzwans-mbp-2.lan>
2022-08-10 15:23:13 +0800

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,15 +36,15 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/omgsian/ridzwanblog/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/omgsian/ux-portfolio/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/omgsian/ridzwanblog/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/omgsian/ux-portfolio/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
