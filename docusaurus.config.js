621b2eed411faae822f9cbb8a6ce5e0593b790ea
Ridzwan Haron <omgwhut@lontong.lan>
2022-08-19 15:06:00 +0800

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,18 +38,16 @@ const config = {
           anonymizeIP: true,
         },
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve('./sidebars.js'),          
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/omgsian/ux-portfolio/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/omgsian/ux-portfolio/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/omgsian/ux-portfolio/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/omgsian/ux-portfolio/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -59,7 +57,6 @@ const config = {
   ],
   plugins: ["docusaurus-plugin-clarity"],
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
         title: 'Ridzwan Haron',
